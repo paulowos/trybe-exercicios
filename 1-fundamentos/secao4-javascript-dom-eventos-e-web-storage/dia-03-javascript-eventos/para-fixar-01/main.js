5fe03b717ffe3c2225725461ea3a770010f44413
paulowos <paulowos@gmail.com>
2023-02-02 10:01:28 -0300

    Realização do requisito 2

--- 1-fundamentos/secao4-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/para-fixar-01/main.js
+++ 1-fundamentos/secao4-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/para-fixar-01/main.js
@@ -25,6 +25,11 @@ thirdLi.addEventListener('click', changeClass);
 // - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('input', (event) => {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+});
+
 // - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 //  - Que tal redirecionar para seu portfólio?
@@ -43,6 +48,9 @@ const resetText = (event) => {
 };
 
 firstLi.addEventListener('dblclick', resetText);
+secondLi.addEventListener('dblclick', resetText);
+thirdLi.addEventListener('dblclick', resetText);
+
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
