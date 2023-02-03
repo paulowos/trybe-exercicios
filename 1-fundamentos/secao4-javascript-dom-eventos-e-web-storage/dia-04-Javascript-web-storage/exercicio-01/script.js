b87e3ebf4094001d48a748f5686faa176714883b
paulowos <paulowos@gmail.com>
2023-02-03 19:28:58 -0300

    Refatoração do requisito 1

--- 1-fundamentos/secao4-javascript-dom-eventos-e-web-storage/dia-04-Javascript-web-storage/exercicio-01/script.js
+++ 1-fundamentos/secao4-javascript-dom-eventos-e-web-storage/dia-04-Javascript-web-storage/exercicio-01/script.js
@@ -1,8 +1,13 @@
-const backgroundColor = document.querySelectorAll('#backgroundColor button');
 
-for (let button of backgroundColor) {
-    button.addEventListener('click', (event) => {
-        const body = document.getElementsByTagName('body')[0];
-        body.style.backgroundColor = event.target.innerText;
-    });
-}
+const changeBgColor = () => {
+    const backgroundColor = document.querySelectorAll('#backgroundColor button');
+    const body = document.getElementsByTagName('body')[0];
+    body.style.backgroundColor = localStorage.getItem('colorBG');
+    for (let button of backgroundColor) {
+        button.addEventListener('click', (event) => {
+            localStorage.setItem('colorBG', event.target.innerText);
+            body.style.backgroundColor = localStorage.getItem('colorBG');
+        });
+    }
+};
+changeBgColor();
