83f9ada762be9ed8ed93ca26273dfe23c5749d78
paulowos <paulowos@gmail.com>
2023-02-14 22:19:30 -0300

    Criação do menu dropdown chapters

--- 1-fundamentos/secao5-HTML-e-CSS-forms-flexbox-e-responsivo/dia-05-CSS-responsivo-mobile-first/exercicio-02/alice.js
+++ 1-fundamentos/secao5-HTML-e-CSS-forms-flexbox-e-responsivo/dia-05-CSS-responsivo-mobile-first/exercicio-02/alice.js
@@ -1,6 +1,21 @@
-const nav = document.querySelector('#menu-items');
-const menu = document.getElementById('menu');
+const menuDropdown = () => {
+    const nav = document.querySelector('#menu-items');
+    const menu = document.getElementById('menu');
 
-menu.addEventListener('click', () => {
-    nav.style.display = nav.style.display === 'none' ? '' : 'none';
-});
+    menu.addEventListener('click', () => {
+        nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+    });
+};
+menuDropdown();
+
+const chapterDropdown = () => {
+    const chapter = document.getElementById('chapters-dropdown');
+    const list = document.querySelector('.aside-one ul');
+
+    chapter.addEventListener('click', () => {
+        list.style.display = list.style.display === 'block' ? 'none' : 'block';
+    });
+    return list;
+};
+
+chapterDropdown();
