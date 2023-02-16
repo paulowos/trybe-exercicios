021aecc1dbb30dee39ad9f3f0e878836875194da
paulowos <paulowos@gmail.com>
2023-02-16 16:14:33 -0300

    Realização do requisito 1 do exercicio 2

---
+++ 1-fundamentos/secao6-introdução-à-JavaScript-ES6-e-testes-unitários/dia-01-fluxo-de-exceções-e-manipulação-de-objetos/exercicio-02/script.js
@@ -0,0 +1,56 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+        street: 'Rua das Flores',
+        number: '389',
+        apartment: '701',
+    },
+    order: {
+        pizza: {
+            marguerita: {
+                amount: 1,
+                price: 25,
+            },
+            pepperoni: {
+                amount: 1,
+                price: 20,
+            },
+        },
+        drinks: {
+            coke: {
+                type: 'Coca-Cola Zero',
+                price: 10,
+                amount: 1,
+            },
+        },
+        delivery: {
+            deliveryPerson: 'Ana Silveira',
+            price: 5,
+        },
+    },
+    payment: {
+        total: 60,
+    },
+};
+
+const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
+    const motoboy = order['order']['delivery']['deliveryPerson'];
+    const name = order['name'];
+    const phoneNumber = order['phoneNumber'];
+    const street = order['address']['street'];
+    const streetNumber = order['address']['number'];
+    const apartment = order['address']['apartment'];
+    const address = `R. ${street}, Nº: ${streetNumber}, AP: ${apartment}`;
+    const message = `Olá ${motoboy}, entrega para: ${name}, Telefone: ${phoneNumber}, ${address}`;
+    console.log(message);
+};
+customerInfo(order);
+
+const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+
+};
+
+orderModifier(order);
