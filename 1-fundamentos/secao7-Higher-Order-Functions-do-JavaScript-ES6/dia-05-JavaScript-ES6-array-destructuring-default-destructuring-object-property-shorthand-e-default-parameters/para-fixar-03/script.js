1f93fcdd2c03b3b1e7169e92e875b4aa5bc6a472
paulowos <paulowos@gmail.com>
2023-03-08 09:37:42 -0300

    Add object shorthand syntax to getPosition function

---
+++ 1-fundamentos/secao7-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar-03/script.js
@@ -0,0 +1,11 @@
+// const getPosition = (latitude, longitude) => ({
+//   latitude: latitude,
+//   longitude: longitude,
+// });
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
