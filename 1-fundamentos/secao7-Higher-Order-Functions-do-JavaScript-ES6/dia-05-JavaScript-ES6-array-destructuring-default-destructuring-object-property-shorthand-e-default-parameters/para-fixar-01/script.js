acecd26cdf133f25ba7bf09ffbffe984c12a6dc9
paulowos <paulowos@gmail.com>
2023-03-08 09:30:44 -0300

    Update script.js to use array destructuring to produce the expected result of the console.log

--- 1-fundamentos/secao7-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar-01/script.js
+++ 1-fundamentos/secao7-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar-01/script.js
@@ -25,3 +25,15 @@ console.log(comida, animal, bebida); // arroz gato água
 [comida, animal, bebida] = ['arroz', 'gato', 'agua'];
 
 console.log(comida, animal, bebida);
+
+//Item 03
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+
+[, , , ...numerosPares] = numerosPares;
+
+console.log(numerosPares);
+
