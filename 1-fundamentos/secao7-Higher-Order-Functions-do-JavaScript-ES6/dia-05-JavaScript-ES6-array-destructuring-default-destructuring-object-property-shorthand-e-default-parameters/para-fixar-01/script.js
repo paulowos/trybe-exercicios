4b41ac8e3114647590c5604461e732e49ae3266c
paulowos <paulowos@gmail.com>
2023-03-08 09:26:57 -0300

    Update script.js to fix array destructuring and add Item 02

--- 1-fundamentos/secao7-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar-01/script.js
+++ 1-fundamentos/secao7-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/para-fixar-01/script.js
@@ -1,3 +1,4 @@
+//Item 01
 const primeNumbers = [17, 23, 37];
 
 const sum = (a, b) => {
@@ -10,5 +11,17 @@ sum(primeNumbers[0], primeNumbers[2]); // 54
 
 const [firstNum, secondNum, thirdNum] = primeNumbers;
 
-sum(firstNum, thirdNum)
+sum(firstNum, thirdNum);
 
+//Item 02
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = ['arroz', 'gato', 'agua'];
+
+console.log(comida, animal, bebida);
