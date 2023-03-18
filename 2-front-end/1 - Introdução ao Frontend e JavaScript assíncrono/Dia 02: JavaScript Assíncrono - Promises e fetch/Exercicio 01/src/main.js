fad7e38837d739a5cb79d6245009a5201c618609
paulowos <paulowos@gmail.com>
2023-03-18 19:20:45 -0300

    Exercicio 1

---
+++ 2-front-end/1 - Introdução ao Frontend e JavaScript assíncrono/Dia 02: JavaScript Assíncrono - Promises e fetch/Exercicio 01/src/main.js
@@ -0,0 +1,28 @@
+import './style.css';
+import Swal from 'sweetalert2';
+
+const randomHeroBtn = document.querySelector('#randomHero');
+const heroImg = document.querySelector('img');
+const heroName = document.querySelector('h1');
+
+
+
+randomHeroBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const id = Math.round(Math.random() * 600);
+  fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${id}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      heroImg.src = `${data.images.md}`;
+      heroName.innerText = `${data.name}`;
+    })
+    .catch(() => Swal.fire(
+      {
+        icon: 'error',
+        title: 'Error',
+        text: 'Hero not Found',
+        background: '#434c5e',
+        color: '#bf616a'
+      }
+    ));
+});
