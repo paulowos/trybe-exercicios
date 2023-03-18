0a6a2f975ff87a31e1de66cd88854598ecf118fc
paulowos <paulowos@gmail.com>
2023-03-18 19:28:58 -0300

    Exercicio 1 Lint Correction

--- 2-front-end/1 - Introdução ao Frontend e JavaScript assíncrono/Dia 02: JavaScript Assíncrono - Promises e fetch/Exercicio 01/src/main.js
+++ 2-front-end/1 - Introdução ao Frontend e JavaScript assíncrono/Dia 02: JavaScript Assíncrono - Promises e fetch/Exercicio 01/src/main.js
@@ -5,11 +5,10 @@ const randomHeroBtn = document.querySelector('#randomHero');
 const heroImg = document.querySelector('img');
 const heroName = document.querySelector('h1');
 
-
-
 randomHeroBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  const id = Math.round(Math.random() * 600);
+  const heroIdLength = 600;
+  const id = Math.round(Math.random() * heroIdLength);
   fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${id}.json`)
     .then((response) => response.json())
     .then((data) => {
@@ -22,7 +21,7 @@ randomHeroBtn.addEventListener('click', (event) => {
         title: 'Error',
         text: 'Hero not Found',
         background: '#434c5e',
-        color: '#bf616a'
-      }
+        color: '#bf616a',
+      },
     ));
 });
