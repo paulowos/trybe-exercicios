213ffec7725c99fb7698f6dff9ab436baea95e86
paulowos <paulowos@gmail.com>
2023-03-29 21:30:56 -0300

    Dia 1

---
+++ 2-front-end/2 - Introdução ao React/Dia 01: 'Hello, world!' no React/Para fixar/src/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Paulo Willian</h1>
+        <p>Olá sou Paulo</p>
+
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+          <li>React</li>
+          <li>JSX</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default About;
