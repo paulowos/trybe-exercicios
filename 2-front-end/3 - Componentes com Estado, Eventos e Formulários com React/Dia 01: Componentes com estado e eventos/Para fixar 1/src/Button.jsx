d92e2b76d33c7fd5bc958f19d7a6ac0990f16920
paulowos <paulowos@gmail.com>
2023-04-03 15:47:46 -0300

    Update Button component to dynamically set background color and implement console log

--- 2-front-end/3 - Componentes com Estado, Eventos e Formulários com React/Dia 01: Componentes com estado e eventos/Para fixar 1/src/Button.jsx
+++ 2-front-end/3 - Componentes com Estado, Eventos e Formulários com React/Dia 01: Componentes com estado e eventos/Para fixar 1/src/Button.jsx
@@ -12,14 +12,31 @@ export default class Button extends Component {
     count3: 0,
   };
 
-  handleClick(key) {
-    this.setState((prev) => ({ [key]: prev[key] + 1 }));
+  setBtnColor(num) {
+    return num % 2 === 0 ? '#cee7cc' : 'grey';
   }
+
+  handleClick(key) {
+    this.setState(
+      (prev) => ({ [key]: prev[key] + 1 }),
+      () => console.log(this.setBtnColor(this.state[key]))
+    );
+  }
+
   render() {
     const { log } = this.props;
     const key = `count${log}`;
+    const style = {
+      backgroundColor: this.setBtnColor(this.state[key]),
+      border: 'none',
+      padding: '20px 40px',
+      margin: 20,
+    };
+
     return (
-      <button onClick={() => this.handleClick(key)}>{this.state[key]}</button>
+      <button onClick={() => this.handleClick(key)} style={style}>
+        {this.state[key]}
+      </button>
     );
   }
 }
