a5bc856238a43c798b14161e04e86e74d56b747f
paulowos <paulowos@gmail.com>
2023-04-04 15:01:29 -0300

    Para fixar 2

--- 2-front-end/3 - Componentes com Estado, Eventos e Formulários com React/Dia 02: Formulários no React/Para fixar 1, 2 e 3/src/Form.jsx
+++ 2-front-end/3 - Componentes com Estado, Eventos e Formulários com React/Dia 02: Formulários no React/Para fixar 1, 2 e 3/src/Form.jsx
@@ -3,38 +3,86 @@ import React, { Component } from 'react';
 class Form extends Component {
   state = {
     name: '',
+    password: '',
+    yesOrNo: 'yes',
+    check: false,
   };
 
   handleChange = ({ target }) => {
-    const { value } = target;
-    this.setState({ name: value });
+    const { name, type } = target;
+    let value = '';
+    if (type === 'checkbox') {
+      value = target.checked;
+    } else if (type === 'file') {
+      value = URL.createObjectURL(target.files[0]);
+    } else {
+      value = target.value;
+    }
+    this.setState({ [name]: value });
   };
 
   render() {
+    const { name, password, yesOrNo, check, photo } = this.state;
     return (
       <form>
-        <label>
-          Nome:
+        <fieldset>
+          <legend>Login</legend>
+          <label htmlFor="name">Nome:</label>
           <input
             type="text"
             name="name"
+            id="name"
             onChange={this.handleChange}
-            value={this.state.name}
+            value={name}
           />
-        </label>
+          <br />
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={this.handleChange}
+            value={password}
+          />
+        </fieldset>
         <br />
-        <label>
-          Senha:
-          <input type="password" name="password" />
-        </label>
-        <br />
-        <label>
-          Sim ou Não:
-          <select name="yesOrNo">
+        <fieldset>
+          <legend>Marcadores</legend>
+          <label htmlFor="yesOrno">Sim ou Não:</label>
+          <select
+            name="yesOrNo"
+            id="yesOrno"
+            onChange={this.handleChange}
+            value={yesOrNo}>
             <option value="yes">Sim</option>
             <option value="no">Não</option>
           </select>
-        </label>
+
+          <br />
+
+          <label htmlFor="check">Marcado?</label>
+          <input
+            type="checkbox"
+            name="check"
+            id="check"
+            onChange={this.handleChange}
+            value={check}
+          />
+        </fieldset>
+
+        <br />
+        <fieldset>
+          <legend>Imagem</legend>
+          <label htmlFor="photo">Foto:</label>
+          <input
+            type="file"
+            name="photo"
+            id="photo"
+            onChange={this.handleChange}
+          />
+          <br />
+          <img src={photo} alt="photo" />
+        </fieldset>
       </form>
     );
   }
