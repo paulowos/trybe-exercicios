c1be1faaefcafa19bf38637b2c5d6a1185628f88
paulowos <paulowos@gmail.com>
2023-10-17 14:53:22 -0300

    exercicios bonus 01 ao 04

---
+++ 4-ciencia-da-computacao/secao1-introducao-a-python/dia-01-aprendendo-python/exercicios-bonus-01-ao-04/01.py
@@ -0,0 +1,7 @@
+def smallestNumber(numbers):
+    return min(numbers)
+
+
+NUMBERS = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 1]
+
+print(smallestNumber(NUMBERS))
---
+++ 4-ciencia-da-computacao/secao1-introducao-a-python/dia-01-aprendendo-python/exercicios-bonus-01-ao-04/02.py
@@ -0,0 +1,6 @@
+def draw_triangle(n):
+    for i in range(n + 1):
+        print("*" * i)
+
+
+draw_triangle(2)
