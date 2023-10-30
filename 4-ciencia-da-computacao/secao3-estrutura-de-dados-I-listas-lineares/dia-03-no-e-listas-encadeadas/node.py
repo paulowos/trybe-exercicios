7502cf00311cb2ea51e63a0aa5c66dbeaf24eabb
paulowos <paulowos@gmail.com>
2023-10-30 19:32:07 -0300

    Exercicios

---
+++ 4-ciencia-da-computacao/secao3-estrutura-de-dados-I-listas-lineares/dia-03-no-e-listas-encadeadas/node.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    def __init__(self, value):
+        self.value = value  # 🎲 Dado a ser armazenado
+        self.next = None  # 👉 Forma de apontar para outro nó
